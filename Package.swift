ae50888e046606192aaf8bf09b712795a8feb4a3
Riccardo Crippa <therickys93@gmail.com>
2022-11-25 19:21:03 +0100

    add other deps.

--- Package.swift
+++ Package.swift
@@ -25,12 +25,14 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FBAuthentication",
-            // dependencies: [
-            //     .product(name: "FirebaseAuth", package: "Firebase"),
-            //     .product(name: "FirebaseFirestore", package: "Firebase"),
-            //     .product(name: "FirebaseFirestoreSwift", package: "Firebase")
-            // ],
-            dependencies: ["firebase-ios-sdk"],
+            dependencies: [
+                .product(name: "FirebaseAuth", package: "Firebase"),
+                .product(name: "FirebaseFirestore", package: "Firebase"),
+                .product(name: "FirebaseFirestoreSwift", package: "Firebase"),
+                .product(name: "FirebaseStorage", package: "Firebase"),
+                .product(name: "FirebaseAnalytics", package: "Firebase"),
+                .product(name: "FirebaseAnalyticsSwift", package: "Firebase")
+            ],
             resources: [.process("Image.xcassets")]),
         .testTarget(
             name: "FBAuthenticationTests",
