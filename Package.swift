270c0674147d9b10e9f826686addf18805e88c5b
Riccardo Crippa <therickys93@gmail.com>
2022-11-25 18:45:36 +0100

    testing

--- Package.swift
+++ Package.swift
@@ -25,11 +25,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "FBAuthentication",
-            dependencies: [
-                .product(name: "FirebaseAuth", package: "Firebase"),
-                .product(name: "FirebaseFirestore", package: "Firebase"),
-                .product(name: "FirebaseFirestoreSwift", package: "Firebase")
-            ],
+            // dependencies: [
+            //     .product(name: "FirebaseAuth", package: "Firebase"),
+            //     .product(name: "FirebaseFirestore", package: "Firebase"),
+            //     .product(name: "FirebaseFirestoreSwift", package: "Firebase")
+            // ],
             resources: [.process("Image.xcassets")]),
         .testTarget(
             name: "FBAuthenticationTests",
