a9e3c100d4279571ab643e7e930386c65b434363
Riccardo Crippa <therickys93@gmail.com>
2022-11-26 16:31:24 +0100

    password reset UI updated.

--- Sources/FBAuthentication/Views/ForgotPasswordView.swift
+++ Sources/FBAuthentication/Views/ForgotPasswordView.swift
@@ -8,28 +8,36 @@
 
 import SwiftUI
 
+private enum FocusableField: Hashable {
+    case email
+}
+
 struct ForgotPasswordView: View {
     @State var user: UserViewModel = UserViewModel()
     @Environment(\.presentationMode) var presentationMode
     @State private var showAlert = false
     @State private var errString: String?
+    @FocusState private var focus: FocusableField?
     var primaryColor: UIColor
     var body: some View {
         NavigationView {
             VStack {
-                TextField("Enter email address", text: $user.email)
-                    .autocapitalization(.none)
-                    .keyboardType(.emailAddress)
-                Button {
-                    FBAuth.resetPassword(email: self.user.email) { (result) in
-                        switch result {
-                        case .failure(let error):
-                            self.errString = error.localizedDescription
-                        case .success:
-                            break
+                HStack {
+                    Image(systemName: "at")
+                    TextField("Enter email address", text: $user.email)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled(true)
+                        .keyboardType(.emailAddress)
+                        .focused($focus, equals: .email)
+                        .submitLabel(.go)
+                        .onSubmit {
+                            resetPassword()
                         }
-                        self.showAlert = true
-                    }
+                }
+                .padding(.vertical, 6)
+                
+                Button {
+                    resetPassword()
                 } label: {
                     Text("Reset")
                         .frame(width: 200)
@@ -41,20 +49,42 @@ struct ForgotPasswordView: View {
                 }
                 .disabled(!user.isEmailValid( user.email))
                 Spacer()
-            }.padding(.top)
-                .frame(width: 300)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-            .navigationBarTitle("Request a password reset", displayMode: .inline)
-                .navigationBarItems(trailing: Button("Dismiss") {
+            }
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .navigationTitle("Request a password reset")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button {
+                        self.presentationMode.wrappedValue.dismiss()
+                    } label: {
+                        Image(systemName: "xmark.circle")
+                            .foregroundColor(Color(primaryColor))
+                    }
+                }
+            }
+            .alert(isPresented: $showAlert) {
+                Alert(title: Text("Password Reset"),
+                  message: Text(self.errString ?? "Success. Reset email sent successfully.  Check your email"),
+                  dismissButton: .default(Text("OK")) {
                     self.presentationMode.wrappedValue.dismiss()
                 })
-                .alert(isPresented: $showAlert) {
-                    Alert(title: Text("Password Reset"),
-                          message: Text(self.errString ?? "Success. Reset email sent successfully.  Check your email"),
-                          dismissButton: .default(Text("OK")) {
-                            self.presentationMode.wrappedValue.dismiss()
-                        })
             }
+        }
+    }
+}
+
+extension ForgotPasswordView {
+    func resetPassword() {
+        FBAuth.resetPassword(email: self.user.email) { (result) in
+            switch result {
+            case .failure(let error):
+                self.errString = error.localizedDescription
+            case .success:
+                break
+            }
+            self.showAlert = true
         }
     }
 }
